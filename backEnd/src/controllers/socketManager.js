b6391eafe4e73b8ba535bc83212f111ce1302785
ishban2006 <ishaanbansal222@gmail.com>
2026-06-23 22:53:23 +0530

    Created socketMgmt. functions

--- backEnd/src/controllers/socketManager.js
+++ backEnd/src/controllers/socketManager.js
@@ -1,10 +1,101 @@
 const { Server } = require("socket.io");
 
+let connections = {};       // Kitne Log
+let messages = {};          // Chats during meet
+let timeOnline = {};        
+
 module.exports.connectToSocket = (server) => {
-    return new Server(server, {
+    const io = new Server(server, {
         cors: {
             origin: "http://localhost:5173",
             methods: ["GET", "POST"]
         }
     });
+
+    io.on("connection", (socket) => {       // Whenever a connection comes
+        socket.on("join-meet", (path) => {
+            if (connections[path] === undefined) {
+                connections[path] = [];         // Create Room if dne
+            }
+
+            connections[path].push(socket.id);          // Socket.id is current live connection id of user
+            timeOnline[socket.id] = new Date();
+
+            connections[path].forEach(elem => {
+                io.to(elem).emit("User Joined", socket.id, connections[path]);
+            });            
+            if (messages[path] !== undefined) {
+                messages[path].forEach(elem => {
+                    io.to(socket.id).emit("chat-message", elem.data, 
+                        elem.sender, elem['sender-socket-id']);
+                });
+            }
+        });
+
+        socket.on("notify", (toId, message) => {
+            io.to(toId).emit("notify", socket.id, message);
+        });
+
+        socket.on("chat", (data, sender) => {
+            const [matchingRoom, userFound] = Object.entries(connections)
+                .reduce(([room, isFound], [roomKey, roomVal]) => {
+
+                    if (!isFound && roomVal.includes(socket.id)) {
+                        return [roomKey, true];
+                    }
+                    return [room, isFound];
+                }, ['', false]);
+
+            if (userFound) {
+                if (messages[matchingRoom] === undefined) {
+                    messages[matchingRoom] = [];
+                }
+
+                messages[matchingRoom].push({
+                    data,
+                    sender,
+                    'sender-socket-id': socket.id
+                });
+
+                connections[matchingRoom].forEach(elem => {
+                    io.to(elem).emit("chat-message", data, sender, socket.id);
+                });
+            }
+        });
+
+        socket.on("disconnect", () => {
+            const [matchingRoom, userFound] = Object.entries(connections)
+            .reduce(([room, isFound], [roomKey, roomVal]) => {
+
+                    if (!isFound && roomVal.includes(socket.id)) {
+                        return [roomKey, true];
+                    }
+
+                    return [room, isFound];
+                }, ['', false]);
+
+            if (userFound) {
+                const diffTime = Math.abs(
+                    timeOnline[socket.id] - new Date()
+                );
+
+                connections[matchingRoom].forEach(elem => {
+                    io.to(elem).emit(
+                        "user-left",
+                        socket.id
+                    );
+                });
+                connections[matchingRoom] = connections[matchingRoom]
+                .filter(id => id !== socket.id);
+
+                if (connections[matchingRoom].length === 0) {
+                    delete connections[matchingRoom];               // If room is empty drop it
+                }
+
+                delete timeOnline[socket.id];
+            }
+        });
+    });
+
+    return io;
 };
