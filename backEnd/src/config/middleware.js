70164b5339729aa1b8824319aceaaf7f646dc0dc
ishban2006 <ishaanbansal222@gmail.com>
2026-06-24 15:28:06 +0530

    Removed incorrect statement from middleware

--- backEnd/src/config/middleware.js
+++ backEnd/src/config/middleware.js
@@ -1,5 +1,4 @@
 module.exports.dispError = (err, req, res, next) => {
-    console.log("Error Handled : ", message);
     let {statusCode = 500, message = "Something Went Wrong"} = err;
 
     return res.status(statusCode).json({ message });
